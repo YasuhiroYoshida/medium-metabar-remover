780e9fff8f3132714124adfefaffdef28b815cfd
Yasuhiro Yoshida <892791+YasuhiroYoshida@users.noreply.github.com>
2018-07-25 14:35:06 +0300

    Accommodate domains other than medium.com

--- contentScript.js
+++ contentScript.js
@@ -1,1 +1,4 @@
-document.getElementsByClassName("metabar")[0].style.display = "none";
+var metabars = document.getElementsByClassName("metabar");
+Array.from(metabars).forEach(function(metabar) {
+  metabar.style.display = "none";
+});
